284b16b94e3004f758deb242c07e697cb4bf894c
Chris <cachapline8@gmail.com>
2025-01-09 21:37:48 -0700

    Rewrite to use slash commands (#1)
    * Migrate to using slash commands
    
    * Remove stuff that isn't used anymore
    
    * More cleanup, error handling
    
    * Add docker image workflow
    
    * Move dockerfile
    
    * Fix typo
    
    * Add some error logging
    
    * Code cleanup, support env var for token
    
    ---------
    
    Co-authored-by: gizmo385 <gizmo385@users.noreply.github.com>

---
+++ commands/magic/card.js
@@ -0,0 +1,34 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js')
+const ResponseTypes = require('../../lib/response-types.js')
+
+const data = new SlashCommandBuilder()
+	.setName("card")
+	.setDescription("Load information about a magic card")
+	.addStringOption(option =>
+		option
+			.setName("name")
+			.setDescription("The name of the magic card")
+			.setRequired(true))
+	.addBooleanOption(option =>
+		option
+			.setName("image")
+			.setDescription("Whether to only fetch the image of the card")
+			.setRequired(false))
+
+
+async function execute(interaction) {
+	const card_name = interaction.options.getString("name");
+	const only_send_image = interaction.options.getBoolean("image") ?? false;
+	const responseClass = only_send_image ? ResponseTypes.ImageResponse : ResponseTypes.TextResponse
+	const response = new responseClass(card_name)
+
+	try {
+		const embed = await response.embed()
+		await interaction.reply({ embeds: [embed]})
+	} catch (error) {
+		console.log(error)
+		await interaction.reply("Error running the search")
+	}
+}
+
+module.exports = {data, execute}
--- index.js
+++ index.js
@@ -1,10 +1,70 @@
-var Discord = require("discord.js");
-const Servo = require('./lib/bot');
+const { Client, Collection, Events, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
+const fs = require('node:fs')
+const path = require('node:path')
+const { token } = require('./config.json')
 
+const discordToken = token || process.env.DISCORD_TOKEN
 
-if (!process.env.DISCORD_TOKEN) {
-  console.log('Error: Specify SLACK_TOKEN in environment');
+// Validate that there is a token in the config
+if (!discordToken) {
+  console.log('Error: Please add a `token` to `config.json`');
   process.exit(1);
 }
 
-new Servo(process.env.DISCORD_TOKEN);
+// Set up the client
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+const options = {
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	]
+}
+
+const client = new Client(options);
+client.commands = new Collection();
+
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) {
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		}
+	}
+});
+
+client.once(Events.ClientReady, readyClient => {
+	console.log("Logged in")
+})
+
+// Connect to Discord
+client.login(discordToken)
